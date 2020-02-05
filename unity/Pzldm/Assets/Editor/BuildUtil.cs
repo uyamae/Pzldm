605531c18acfc9dbad2c98cd018362e324d34f0b
uyamae <uyamae@gmail.com>
2020-02-05 21:20:14 +0900

    ビルドスクリプト追加

---
+++ unity/Pzldm/Assets/Editor/BuildUtil.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Pzldm
+{
+    public class BuildUtil
+    {
+        [MenuItem("Pzldm/Build Exe")]
+        public static void BuildExe()
+        {
+            Build(BuildTarget.StandaloneWindows64);
+        }
+        private static Dictionary<BuildTarget, string> ExtensionTable = new Dictionary<BuildTarget, string>()
+        {
+            { BuildTarget.StandaloneWindows, "exe" },
+            { BuildTarget.StandaloneWindows64, "exe" },
+            { BuildTarget.iOS, "ipa" },
+            { BuildTarget.Android, "apk" },
+        };
+        private static void Build(BuildTarget target)
+        {
+            string[] scenes = new string[] {
+                "Assets/Scenes/Boot/boot.unity",
+                "Assets/Scenes/Select/select.unity",
+                "Assets/Scenes/Battle/battle.unity"
+            };
+            string ext = ExtensionTable.ContainsKey(target) ? ExtensionTable[target] : "exe";
+            string path = $"output/{target.ToString()}/Pzldm.{ext}";
+            BuildOptions opts = BuildOptions.None;
+            BuildPipeline.BuildPlayer(scenes, path, target, opts);
+        }
+    }
+}
